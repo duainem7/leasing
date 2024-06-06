a9bbee9f720c22fe7646b65f28cc5a3a52c146ca
D Miller <duainem7@gmail.com>
2024-06-06 11:54:06 -0400

    push

--- src/SecondSec.js
+++ src/SecondSec.js
@@ -1,7 +1,7 @@
 import React from "react"
 import image_two from "./images/im2.jpg"
 import Card from 'react-bootstrap/Card'
-import Col from 'react-bootstrap/Col';
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Image from 'react-bootstrap/Image';
