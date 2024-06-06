95ffe27897b922543cfa86a5bede57314bd46643
D Miller <duainem7@gmail.com>
2024-06-06 13:58:07 -0400

    push

--- src/SecondSec.js
+++ src/SecondSec.js
@@ -5,6 +5,7 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Image from 'react-bootstrap/Image';
+import './App.css';
 
 
 
